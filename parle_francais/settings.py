4569cb735b2d235fe3edfbb53c1f6dd77797b1b6
shmouel1824 <b747800613@gmail.com>
2026-05-05 09:20:57 +0300

    Fix-static-storage

--- parle_francais/settings.py
+++ parle_francais/settings.py
@@ -73,7 +73,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 # ── Static & Media files ─────────────────────────────────────
 STATIC_URL  = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
