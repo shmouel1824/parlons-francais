4a5e8987473ba53e922b7c71b757acf28c5945ea
shmouel1824 <b747800613@gmail.com>
2026-05-05 13:09:14 +0300

    Fix-start-script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn parle_francais.wsgi --bind 0.0.0.0:${PORT:-8000}
+set -e
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput || true
+exec gunicorn parle_francais.wsgi --bind 0.0.0.0:${PORT:-8000} --workers 2
